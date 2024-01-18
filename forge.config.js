1e3d234dce002fbe2895f54bce772fca2e8de157
Rémi Sarrailh <remi@madnerd.org>
2024-01-18 19:13:29 +0100

    Modification for windows installer

--- forge.config.js
+++ forge.config.js
@@ -1,3 +1,7 @@
+const path = require("path");
+const FFmpegStatic = require("ffmpeg-static-electron-forge").default;
+
+
 module.exports = {
   packagerConfig: {
     asar: true,
@@ -6,7 +10,10 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        certificateFile: './cert.pfx',
+        certificatePassword: process.env.CERTIFICATE_PASSWORD
+      },
     },
     {
       name: '@electron-forge/maker-zip',
@@ -26,5 +33,28 @@ module.exports = {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
+    {
+      name: "@electron-forge/plugin-webpack",
+      config: {
+        mainConfig: "./webpack.main.config.js",
+        renderer: {
+          config: "./webpack.renderer.config.js",
+          entryPoints: [
+            {
+              html: "./src/index.html",
+              js: "./src/renderer.js",
+              name: "main_window",
+              preload: {
+                js: "./src/preload.js",
+              },
+            },
+          ],
+        },
+      },
+    },
+    new FFmpegStatic({
+      remove: true, // Required
+      path: path.join(__dirname, ".webpack", "main"), // Set path of main build
+    }),
   ],
 };
